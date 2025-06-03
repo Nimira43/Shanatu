7c5473e2041ea8c90608febc9b86d85a94d1405f
Nimira43 <n.rabone72@aol.com>
2025-06-03 10:03:24 +0100

    Basic login page.

---
+++ app/(auth)/login/[[...login]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from '@clerk/nextjs'
+
+export default function LoginPage() {
+  return (
+    <main className='flex flex-col items-center p-5 gap-10 animate-fade-in'>
+      <h1 className='logo text-4xl'>Shanatu</h1>
+      <div className='mt-3'>
+        <SignIn />
+      </div>
+    </main>
+  )
+}
